e2696bb1d21c057dba66edec9d650434b72393f5
pedridev <pedridevgh@gmail.com>
2024-08-27 21:26:01 -0300

    exception

---
+++ excecao/verificada/LeitorDocumentos.java
@@ -0,0 +1,10 @@
+package excecao.verificada;
+
+import java.io.BufferedReader;
+import java.nio.Buffer;
+
+public class LeitorDocumentos {
+    public static void main(String[] args){
+        BufferedReader bufferedReader = null;
+    }
+}
