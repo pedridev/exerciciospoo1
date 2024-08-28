6b0f2320509608ee09eabfd474ecbb2df731b3e8
pedridev <pedridevgh@gmail.com>
2024-08-27 22:28:45 -0300

    exception try catch

--- excecao/verificada/LeitorDocumentos.java
+++ excecao/verificada/LeitorDocumentos.java
@@ -1,10 +1,25 @@
 package excecao.verificada;
 
 import java.io.BufferedReader;
-import java.nio.Buffer;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LeitorDocumentos {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("C:\\Users\\CAXETA\\OneDrive\\Área de Trabalho\\metodo1.txt"));
+
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo." + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
